26f0bd87dfab40517d2d33d094cf2aa9d6e419f2
Armin <halilovic.armin@hotmail.com>
2016-04-24 11:38:00 +0200

    impoved visualise script

--- src/visualize.sh
+++ src/visualize.sh
@@ -1,4 +1,18 @@
+# create files for java tree visualisation
 java -jar ../resources/antlr-4.5.2-complete.jar SmallC.g4 -visitor
-java -jar ../resources/antlr-4.5.2-complete.jar -Dlanguage=Python3 SmallC.g4 -visitor
 javac -classpath ../resources/antlr-4.5.2-complete.jar SmallC*.java
-cat testfiles/hello_world.c | java -cp ../resources/antlr-4.5.2-complete.jar: org.antlr.v4.gui.TestRig SmallC program -gui
+
+# move java files to build dir
+if [ -d "build" ]; then
+    rm -r build/*
+else
+    mkdir build
+fi
+mv *.java *.class build/
+
+# create python files
+java -jar ../resources/antlr-4.5.2-complete.jar -Dlanguage=Python3 SmallC.g4 -visitor
+
+# run visualisation
+(cd build && java -cp ../../resources/antlr-4.5.2-complete.jar: org.antlr.v4.gui.TestRig SmallC program -gui ../testfiles/hello_world.c)
+#(cd build && java -cp ../../resources/antlr-4.5.2-complete.jar: org.antlr.v4.gui.TestRig SmallC program -gui ../testfiles/hello_world0.c ../testfiles/hello_world.c )
