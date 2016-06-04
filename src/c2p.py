f3d80fef7fb3414b34b44378eddb76e23533b5d7
Armin <halilovic.armin@hotmail.com>
2016-06-04 16:34:01 +0200

    implemented main program flags, updated main file and todo

--- src/c2p.py
+++ src/c2p.py
@@ -14,34 +14,41 @@ import argparse
 import traceback
 import sys
 import time
+import os.path
 
-arg_save_ast          = False
-arg_save_symbol_table = False
-arg_timings           = False
-arg_quiet             = False
-arg_out_filename      = "out.p"
 
-def output(text, print_arg=None):
-    if print_arg is not None:
-        if print_arg:
-            print(text)
-    elif not arg_quiet:
-        print(text)
+# GLOBAL VARIABLES
+SAVE_AST          = False
+SAVE_SYMBOL_TABLE = False
+PRINT_TIMINGS     = False
+PRINT_NOTHING     = False
+OUT_FILE_NAME     = "out.p"
 
-def main(filename):
+
+def output(text, is_timing=False):
+    if PRINT_NOTHING:
+        return
+
+    if is_timing and not PRINT_TIMINGS:
+        return
+
+    print(text)
+
+
+def parseFile(filename):
     timeNow = time.time()
     input_file = FileStream(filename)
-    output("file read:            " + str(time.time() - timeNow), arg_timings)
+    output("file read:            " + str(time.time() - timeNow), is_timing=True)
 
     # get lexer
     timeNow = time.time()
     lexer = SmallCLexer(input_file)
-    output("file lexed:           " + str(time.time() - timeNow), arg_timings)
+    output("file lexed:           " + str(time.time() - timeNow), is_timing=True)
 
     # get list of matched tokens
     timeNow = time.time()
     stream = CommonTokenStream(lexer)
-    output("file tokenized:       " + str(time.time() - timeNow), arg_timings)
+    output("file tokenized:       " + str(time.time() - timeNow), is_timing=True)
 
     # pass tokens to the parser
     parser = SmallCParser(stream)
@@ -49,58 +56,100 @@ def main(filename):
     # specify the entry point
     timeNow = time.time()
     programContext = parser.program() # tree with program as root
-    output("file parsed:          " + str(time.time() - timeNow), arg_timings)
+    output("file parsed:          " + str(time.time() - timeNow), is_timing=True)
 
     # quit if there are any syntax errors
     if parser._syntaxErrors > 0:
         sys.exit(0)
 
-    # walk it and attach our listener
-    walker = ParseTreeWalker()
+    return programContext
+
+
+def buildAST(parseTreeRoot):
+    timeNow = time.time()
 
     # create an AST an attach it to a listener so the listener can fill in the tree
-    abstractSyntaxTree = AbstractSyntaxTree();
+    abstractSyntaxTree = AbstractSyntaxTree()
+
+    walker = ParseTreeWalker()
+    listener = Listener(abstractSyntaxTree)
+    # attach the listener, walk the parse tree, and fill in the AST
+    walker.walk(listener, parseTreeRoot)
+
+    # output the resulting AST after the walk
+    output("AST built:            " + str(time.time() - timeNow), is_timing=True)
+    output(str(abstractSyntaxTree))
+
+    if SAVE_AST:
+        # os.path.splitext(name) splits name into tuple: (name without extension, extension)
+        filename = os.path.splitext(OUT_FILE_NAME)[0] + "_AST.txt"
+        outfile = open(filename, 'w')
+        outfile.write(str(abstractSyntaxTree))
+        outfile.close()
+
+    return abstractSyntaxTree
+
+
+def scopeCheck(abstractSyntaxTree, errorHandler, symbolTable):
+    timeNow = time.time()
+    functionFiller = VisitorDefinitionProcessor(symbolTable, errorHandler)
+    functionFiller.visitProgramNode(abstractSyntaxTree.root)
+    output("symbol table filled:  " + str(time.time() - timeNow), is_timing=True)
+
+    symbolTable.traverseOn()
+    symbolTable.resetToRoot()
+
+    timeNow = time.time()
+    tableFiller = VisitorDeclarationProcessor(symbolTable, errorHandler)
+    tableFiller.visitProgramNode(abstractSyntaxTree.root)
+    output("symbol table checked: " + str(time.time() - timeNow), is_timing=True)
+    output(str(symbolTable))
+
+    if SAVE_SYMBOL_TABLE:
+        # os.path.splitext(name) splits name into tuple: (name without extension, extension)
+        filename = os.path.splitext(OUT_FILE_NAME)[0] + "_symbol_table.txt"
+        outfile = open(filename, 'w')
+        outfile.write(str(symbolTable))
+        outfile.close()
+
+
+def typeCheck(abstractSyntaxTree, errorHandler):
+    timeNow = time.time()
+    typeCheck = VisitorTypeChecker(errorHandler)
+    typeCheck.visitProgramNode(abstractSyntaxTree.root)
+    output("program type checked: " + str(time.time() - timeNow), is_timing=True)
+
+
+def generateCode(abstractSyntaxTree, symbolTable):
+    timeNow = time.time()
+    codeGenerator = VisitorCodeGenerator(symbolTable, OUT_FILE_NAME)
+    codeGenerator.visitProgramNode(abstractSyntaxTree.root)
+    output("code generated:       " + str(time.time() - timeNow), is_timing=True)
+
+
+def main(filename):
+    # get the root of the parse tree of the input file
+    parseTreeRoot = parseFile(filename)
 
     # the errorHandler which will group all of the errors
     errorHandler = CompilerErrorHandler(filename)
 
     try:
-        listener = Listener(abstractSyntaxTree)
-        # walk the parse tree and fill in the AST
-        timeNow = time.time()
-        walker.walk(listener, programContext)
-        output("AST built:            " + str(time.time() - timeNow), arg_timings)
+        # create an AST an attach it to a listener so the listener can fill in the tree
+        abstractSyntaxTree = buildAST(parseTreeRoot)
 
-        # output the resulting AST after the walk
         output(str(abstractSyntaxTree))
-
         # create a symbol table and symbol table filler, fill in the table and check if everything is declared before it is used in the c file
         symbolTable = SymbolTable()
-        timeNow = time.time()
-        functionFiller = VisitorDefinitionProcessor(symbolTable, errorHandler)
-        functionFiller.visitProgramNode(abstractSyntaxTree.root)
-        output("symbol table filled:  " + str(time.time() - timeNow), arg_timings)
-        symbolTable.traverseOn()
-        symbolTable.resetToRoot()
-        timeNow = time.time()
-        tableFiller = VisitorDeclarationProcessor(symbolTable, errorHandler)
-        tableFiller.visitProgramNode(abstractSyntaxTree.root)
-        output("symbol table checked: " + str(time.time() - timeNow), arg_timings)
-        output(str(symbolTable))
+        scopeCheck(abstractSyntaxTree, errorHandler, symbolTable)
 
         # do the type checking
-        timeNow = time.time()
-        typeCheck = VisitorTypeChecker(errorHandler)
-        typeCheck.visitProgramNode(abstractSyntaxTree.root)
-        output("program type checked: " + str(time.time() - timeNow), arg_timings)
+        typeCheck(abstractSyntaxTree, errorHandler)
 
         # generate code
+        symbolTable.resetToRoot()
         if not errorHandler.errorCount():
-            symbolTable.resetToRoot()
-            timeNow = time.time()
-            codeGenerator = VisitorCodeGenerator(symbolTable, arg_out_filename)
-            codeGenerator.visitProgramNode(abstractSyntaxTree.root)
-            output("code generated:       " + str(time.time() - timeNow), arg_timings)
+            generateCode(abstractSyntaxTree, symbolTable)
 
     except Exception as e:
         ex_type, ex, tb = sys.exc_info()
@@ -112,23 +161,22 @@ def main(filename):
 
 
 if __name__=="__main__":
-
     argparser = argparse.ArgumentParser(description="A C to P compiler")
 
     argparser.add_argument("filename",                                         help="The filename of the c program")
-    argparser.add_argument("-save-ast", "--save-ast", "-saveast", "--saveast", help="Serializes the AST and saves it to c2p_AST.txt", action="store_true", default=False) # saveast as per assignment constraints
-    argparser.add_argument("-save-symbol-table", "--save-symbol-table",        help="Serializes the symbol table and saves it to c2p_symbol_table.txt", action="store_true", default=False)
+    # saveast as per assignment constraint
+    argparser.add_argument("-save-ast", "--save-ast", "-saveast", "--saveast", help="Serializes the AST and saves it to {OUTFILE}_AST.txt", action="store_true", default=False)
+    argparser.add_argument("-save-symbol-table", "--save-symbol-table",        help="Serializes the symbol table and saves it to {OUTFILE}_symbol_table.txt", action="store_true", default=False)
     argparser.add_argument("-t", "--timings",                                  help="Shows how long each step of the process takes", action="store_true", default=False)
     argparser.add_argument("-q", "--quiet",                                    help="Disables the printing of the AST and symbol table", action="store_true", default=False)
     argparser.add_argument("-o",                                               help="Specifies the output filename (preferably with .p filename extension)", default="out.p")
     args = argparser.parse_args()
 
-    arg_save_ast = args.save_ast
-    arg_save_symbol_table = args.save_symbol_table
-    arg_timings = args.timings
-    arg_quiet  = args.quiet
-    arg_out_filename = args.o
-
-    # output("args: " + str(args) + "\n" + "save_ast: " + str(arg_save_ast) + ", save_symbl_table: " + str(arg_save_symbol_table) + ", timings: " + str(arg_timings) + ", quiet: " + str(arg_quiet) + "\n")
+    # set global variables
+    SAVE_AST          = args.save_ast
+    SAVE_SYMBOL_TABLE = args.save_symbol_table
+    PRINT_TIMINGS     = args.timings
+    PRINT_NOTHING     = args.quiet
+    OUT_FILE_NAME     = args.o
 
     main(args.filename)
